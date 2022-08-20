27106219b7e1f1720c5c60a9c09ebdfc4492958b
jde-cpp <johnduffyx@outlook.com>
2022-08-20 18:42:31 -0400

    name collision with framework FromXX.proto

--- source/pc.h
+++ source/pc.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <jde/TypeDefs.h>
-#include "types/proto/FromServer.pb.h"
-#include "types/proto/FromClient.pb.h"
+#include "types/proto/IotFromServer.pb.h"
+#include "types/proto/IotFromClient.pb.h"
 #include "types/FromServer.h"
 #include "../../Framework/source/DateTime.h"
 #include "../../Framework/source/db/GraphQL.h"
